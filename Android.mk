7564155f1038ed3ec8cd46f4216d860310241777
Marc Bourgoin <themard85@gmail.com>
2024-11-23 08:51:35 -0700

    tundra: Initial import of vendor blobs
    from tundra_g-user 14 U1SJS34.2-92-10-2 263481-6419b2 release-keys
    
    Up to commit bc34426dc0106ccb7235ab07138556bc57a28755

---
+++ Android.mk
@@ -0,0 +1,29 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),tundra)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,6e86cb8d19275f48a7c2b774f3cd3180c48729ad)
+$(call add-radio-file-sha1-checked,radio/aop.img,d331ab8f158280917d81c61635b9386b6162967a)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,f9be694e7f516cc1e1991a132a483d652a918daf)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,7d6d965639d91036efb4fb6832b9e071f7e825ed)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,26955f7e9d202e8fb898812fa20a7081cbdcc4cb)
+$(call add-radio-file-sha1-checked,radio/dsp.img,905ecf44147441699bcaffe4dded13b02dc35b4a)
+$(call add-radio-file-sha1-checked,radio/fsg.img,2540811b35f5c20302ba023973e3a12dc46d4228)
+$(call add-radio-file-sha1-checked,radio/hyp.img,f98d6d745601bf42287d40f89d74d949faa2b7ad)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,20bea37e9cf39d00c0a1e27adfae75f19adb08da)
+$(call add-radio-file-sha1-checked,radio/logo.img,b044dd3d6cdda1d71479de974130a3c4e5396dc5)
+$(call add-radio-file-sha1-checked,radio/modem.img,f16e39c66c31f9ae8862c0e89c94a392aa48b885)
+$(call add-radio-file-sha1-checked,radio/prov.img,fd938d483c68df8d369174e014bf9ec2e0f8e668)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,d7fc51d69988940bcf953c7f068077c9948f05f3)
+$(call add-radio-file-sha1-checked,radio/shrm.img,4fe84a9ee46d962e16b2491e29d75fbd2aa3e754)
+$(call add-radio-file-sha1-checked,radio/storsec.img,d8a1eff16e659fefaf2cd63ec5374aa0474264c4)
+$(call add-radio-file-sha1-checked,radio/tz.img,359d29ef6e551131c0ab0299bdd420edc3835a5a)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,3cdb28fc45f82bc8e009b1244bb53f11d1f91196)
+$(call add-radio-file-sha1-checked,radio/xbl.img,e4dba423d443ec6893aa69693cc09f7d4c980017)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,630450ab665c34c6a377b18191445a0f38d65f6c)
+
+endif
